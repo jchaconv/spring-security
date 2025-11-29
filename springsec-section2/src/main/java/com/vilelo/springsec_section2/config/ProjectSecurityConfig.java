f66b947815a230258f25b292ff073ad7e7dc7e97
Julio Chacon <julio_chacon@epamneoris.com>
2025-11-29 18:48:25 -0500

    feature/section3-managing-users-in-memory in memory users with no password encoder

--- springsec-section2/src/main/java/com/vilelo/springsec_section2/config/ProjectSecurityConfig.java
+++ springsec-section2/src/main/java/com/vilelo/springsec_section2/config/ProjectSecurityConfig.java
@@ -4,7 +4,10 @@ package com.vilelo.springsec_section2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -14,8 +17,6 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
-        //http.authorizeHttpRequests(requests -> requests.anyRequest().denyAll());
-        //http.authorizeHttpRequests(requests -> requests.anyRequest().permitAll());
 
         http.authorizeHttpRequests(requests -> requests
                 .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
@@ -24,10 +25,14 @@ public class ProjectSecurityConfig {
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
 
-        //To disable:
-        //http.formLogin(flc -> flc.disable());
-
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails user = User.withUsername("user1").password("{noop}345").authorities("read").build();
+        UserDetails admin = User.withUsername("admin").password("{noop}5543").authorities("admin").build();
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
 
