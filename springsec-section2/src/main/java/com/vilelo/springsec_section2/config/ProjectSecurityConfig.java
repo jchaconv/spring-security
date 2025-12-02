00d56c26d1f686c3209c029b244ddcbcb22e7909
Julio Chacon <julio_chacon@epamneoris.com>
2025-12-01 21:08:34 -0500

    feature/section4-managing-users-in-database - Using JdbcUserDetailsManager to perform authentication

--- springsec-section2/src/main/java/com/vilelo/springsec_section2/config/ProjectSecurityConfig.java
+++ springsec-section2/src/main/java/com/vilelo/springsec_section2/config/ProjectSecurityConfig.java
@@ -4,13 +4,13 @@ package com.vilelo.springsec_section2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -31,12 +31,8 @@ public class ProjectSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails user = User.withUsername("user").password("{noop}345").authorities("read").build();
-        UserDetails admin = User.withUsername("admin")
-                .password("{bcrypt}$2a$12$Qzn7V9LuTv7ZmTsnWuBt6O8mIbc6vAwYq1V8LiY8Ez2.LnTHEFNUO")
-                .authorities("admin").build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
     }
 
     @Bean
