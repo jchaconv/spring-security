5c52225becf4988044b45f5299b0437c7c56343e
Julio Chacon <julio_chacon@epamneoris.com>
2025-12-01 22:15:17 -0500

    feature/section4-managing-users-in-database - section 4 completed with rest api for register new user

--- springsec-section2/src/main/java/com/vilelo/springsec_section2/config/ProjectSecurityConfig.java
+++ springsec-section2/src/main/java/com/vilelo/springsec_section2/config/ProjectSecurityConfig.java
@@ -4,6 +4,7 @@ package com.vilelo.springsec_section2.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -20,9 +21,10 @@ public class ProjectSecurityConfig {
     @Bean
     SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(requests -> requests
+        http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests
                 .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
-                .requestMatchers("/notices", "/contact", "/error").permitAll());
+                .requestMatchers("/notices", "/contact", "/error", "/register").permitAll());
 
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
