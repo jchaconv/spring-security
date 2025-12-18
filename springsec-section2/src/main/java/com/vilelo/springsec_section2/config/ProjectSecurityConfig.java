2cfeaf6662e2697f90b6380e3f26232589ecff8f
jchaconv <jchacon.vilela@gmail.com>
2025-12-17 22:33:49 -0500

    feature/section7-customization-most-common-use-cases concurrent session code completed without successful tests

--- springsec-section2/src/main/java/com/vilelo/springsec_section2/config/ProjectSecurityConfig.java
+++ springsec-section2/src/main/java/com/vilelo/springsec_section2/config/ProjectSecurityConfig.java
@@ -3,6 +3,7 @@ package com.vilelo.springsec_section2.config;
 
 import com.vilelo.springsec_section2.exceptionhandling.CustomAccessDeniedHandler;
 import com.vilelo.springsec_section2.exceptionhandling.CustomBasicAuthenticationEntryPoint;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -23,9 +24,43 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class ProjectSecurityConfig {
 
     @Bean
-    SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession"))
+                .redirectToHttps(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(requests -> requests
+                        .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLoans").authenticated()
+                        .requestMatchers("/notices", "/contact", "/error", "/register", "/invalidSession").permitAll());
+
+        http.formLogin(form -> form
+                        .failureHandler((request, response, exception) -> {
+                            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+                            if (exception.getMessage().contains("Maximum sessions")) {
+                                response.getWriter().write("Maximum sessions of 1 for this user exceeded");
+                            } else {
+                                response.getWriter().write("Authentication failed");
+                            }
+                        })
+                )
+                .sessionManagement(session -> session
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true));
+
+        http.httpBasic(hbc ->
+                hbc.authenticationEntryPoint(new CustomBasicAuthenticationEntryPoint()));
+
+        http.exceptionHandling(ehc -> ehc.accessDeniedHandler(new CustomAccessDeniedHandler()));
+
+        return http.build();
+    }
+
+
+    /* This code is for old version
+    @Bean
+    SecurityFilterChain defaultFilterChain(HttpSecurity http) throws Exception {
+
+        http.sessionManagement(smc -> smc.invalidSessionUrl("/invalidSession").maximumSessions(1).maxSessionsPreventsLogin(true))
                 .redirectToHttps(AbstractHttpConfigurer::disable) //Only http
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(requests -> requests
@@ -45,6 +80,7 @@ public class ProjectSecurityConfig {
 
         return http.build();
     }
+    */
 
     /*
     @Bean
