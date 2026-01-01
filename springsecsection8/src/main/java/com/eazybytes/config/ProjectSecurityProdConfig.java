6e16bd8eadeebafd33851bcc0ee434dc51b4b303
jchaconv <jchacon.vilela@gmail.com>
2025-12-31 20:25:29 -0500

    feature/section12-method-level-security pre and post authorize test ok

--- springsecsection8/src/main/java/com/eazybytes/config/ProjectSecurityProdConfig.java
+++ springsecsection8/src/main/java/com/eazybytes/config/ProjectSecurityProdConfig.java
@@ -65,7 +65,7 @@ public class ProjectSecurityProdConfig {
                         .requestMatchers("/myCards").hasAuthority("VIEWCARDS")*/
                         .requestMatchers("/myAccount").hasRole("USER")
                         .requestMatchers("/myBalance").hasAnyRole("USER", "ADMIN")
-                        .requestMatchers("/myLoans").hasRole("USER")
+                        .requestMatchers("/myLoans").authenticated()
                         .requestMatchers("/myCards").hasRole("USER")
                         .requestMatchers("/user").authenticated()
                         .requestMatchers("/notices", "/contact", "/error", "/register", "/invalidSession").permitAll());
--- springsecsection8/src/main/java/com/eazybytes/controller/LoansController.java
+++ springsecsection8/src/main/java/com/eazybytes/controller/LoansController.java
@@ -3,6 +3,7 @@ package com.eazybytes.controller;
 import com.eazybytes.model.Loans;
 import com.eazybytes.repository.LoanRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ public class LoansController {
 
     private final LoanRepository loanRepository;
 
+    @PostAuthorize("hasRole('ROOT')")
     @GetMapping("/myLoans")
     public List<Loans> getLoanDetails(@RequestParam long id) {
         List<Loans> loans = loanRepository.findByCustomerIdOrderByStartDtDesc(id);
