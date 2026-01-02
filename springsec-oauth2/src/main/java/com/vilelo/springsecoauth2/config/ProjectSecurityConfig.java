2d3cfecc1bfbc9cc10f92dad33cf4e206973996c
jchaconv <jchacon.vilela@gmail.com>
2026-01-02 11:24:43 -0500

    feature/section14-implementing-oauth2 initial config auth with github test ok

---
+++ springsec-oauth2/src/main/java/com/vilelo/springsecoauth2/config/ProjectSecurityConfig.java
@@ -0,0 +1,42 @@
+package com.vilelo.springsecoauth2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests((requests) -> requests.requestMatchers("/secure").authenticated()
+                .anyRequest().permitAll())
+                .formLogin(Customizer.withDefaults())
+                .oauth2Login(Customizer.withDefaults());
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    ClientRegistrationRepository clientRegistrationRepository() {
+        return new InMemoryClientRegistrationRepository(githubClientRegistration());
+                //, facebookClientRegistration());
+    }
+
+    private ClientRegistration githubClientRegistration() {
+        return CommonOAuth2Provider.GITHUB.getBuilder("github").clientId("Ov23li2g3dLB6GflXIxf")
+                .clientSecret("2a11895ce97376edf20d124a95fb4814840a3a6b").build();
+    }
+
+    /*private ClientRegistration facebookClientRegistration() {
+        return CommonOAuth2Provider.FACEBOOK.getBuilder("facebook").clientId("")
+                .clientSecret("").build();
+    }*/
+
+}
---
+++ springsec-oauth2/src/main/java/com/vilelo/springsecoauth2/controllers/SecureController.java
@@ -0,0 +1,15 @@
+package com.vilelo.springsecoauth2.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SecureController {
+
+    @GetMapping("/secure")
+    public String securePage() {
+        return "secure.html";
+    }
+
+
+}
